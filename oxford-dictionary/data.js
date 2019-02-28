6828674a710cfee28a8458a2cdaa4b2fcf9c3a0b
UstymUkhman <ustym.ukhman@gmail.com>
2019-02-28 20:18:34 +0100

    Oxford Dictionary skill init

---
+++ oxford-dictionary/data.js
@@ -0,0 +1,75 @@
+const ENDPOINT = 'https://od-api.oxforddictionaries.com/api/v1';
+const APP_KEY  = '83a31615121ef0ea3be016ca80da654c';
+const APP_ID  = '85f3fd29';
+
+const https = require('https');
+const word = 'ace';
+
+function parseData (data) {
+  const meanings = [];
+  const result = JSON.parse(data).results[0];
+
+  for (let i = 0; i < result.lexicalEntries.length; i++) {
+    const lexicalEntries = result.lexicalEntries[i].entries;
+
+    for (let j = 0; j < lexicalEntries.length; j++) {
+      const entry = lexicalEntries[j];
+      let etymology = undefined;
+
+      if (entry.etymologies && entry.etymologies.length) {
+        etymology = entry.etymologies[0];
+      }
+
+      for (let k = 0; k < entry.senses.length; k++) {
+        let domain = undefined;
+        const sense = entry.senses[k];
+        const definition = sense.definitions[0];
+
+        if (sense.domains && sense.domains.length) {
+          domain = sense.domains[0];
+        }
+
+        meanings.push({
+          definition: definition,
+          etymology: etymology,
+          domain: domain
+        });
+      }
+    }
+  }
+
+  return meanings;
+}
+
+https.get({
+  host    : 'od-api.oxforddictionaries.com',
+  path    : `/api/v1/entries/en/${word}`,
+  method  : 'GET',
+  port    : 443,
+  headers : {
+    'Accept'  : 'application/json',
+    'app_key' : APP_KEY,
+    'app_id'  : APP_ID
+  }
+}, function (response) {
+  let data = '';
+  let index = 0;
+
+  response.on('data', function (chunk) {
+    data += chunk;
+  })
+
+  .on('end', function () {
+    const meanings = parseData(data);
+    const TOTAL_MEANINGS = meanings.length;
+
+    if (TOTAL_MEANINGS === 0) {
+      // No meanings found
+    } else {
+      playMeaning(meanings[index]);
+      index++;
+    }
+
+    console.log(meanings);
+  })
+})
--- oxford-dictionary/index.js
+++ oxford-dictionary/index.js
@@ -1,60 +1,57 @@
-const ENDPOINT = 'https://od-api.oxforddictionaries.com/api/v1';
-const APP_KEY  = '83a31615121ef0ea3be016ca80da654c';
-const APP_ID  = '85f3fd29';
+/* eslint-disable func-names */
+/* eslint quote-props: ["error", "consistent"] */
 
-const https = require('https');
-const word = 'ace';
 
-https.get({
-  host    : 'od-api.oxforddictionaries.com',
-  path    : `/api/v1/entries/en/${word}`,
-  method  : 'GET',
-  port    : 443,
-  headers : {
-    'Accept'  : 'application/json',
-    'app_key' : APP_KEY,
-    'app_id'  : APP_ID
+'use strict';
+
+const APP_ID   = 'amzn1.ask.skill.b0399b08-77f3-42d4-a8e2-dc6b1b3afd3d';
+// const ENDPOINT = 'https://od-api.oxforddictionaries.com/api/v1';
+const Alexa    = require('alexa-sdk');
+
+const HELP_REPROMPT = 'What word do you want to know the meaning?';
+const HELP          = 'You can say, what does some word mean... or you can say what is some word';
+const UNHANDLED     = 'Sorry, I\'m not sure I understood what you asked... ' + HELP_REPROMPT;
+const WELCOME       = 'Hi! ' + HELP_REPROMPT;
+const STOP          = 'Hope it was helpfull!';
+const CANCEL        = 'OK.';
+
+const handlers = {
+  'LaunchRequest': function () {
+    this.emit(':ask', WELCOME);
+  },
+
+  'AMAZON.HelpIntent': function () {
+    this.emit(':ask', HELP, HELP_REPROMPT);
+  },
+
+  'GiveDefinitionIntent': function () {
+    const word = this.event.request.intent.slots.word.value;
+  
+    this.response.speak('You just said the word ' + word);
+    this.emit(':responseReady');
+    // const _this = this;
+  },
+
+  'Unhandled': function () {
+    this.emit(':ask', UNHANDLED);
+  },
+
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', CANCEL);
+  },
+
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', STOP);
+  },
+
+  'SessionEndedRequest': function () {
+    this.emit(':tell', STOP);
   }
-}, function (response) {
-  let data = '';
-  let meanings = [];
+};
 
-  response.on('data', function(chunk) {
-    data += chunk;
-  })
-
-  .on('end',function () {
-    const result = JSON.parse(data).results[0];
-
-    for (let i = 0; i < result.lexicalEntries.length; i++) {
-      const lexicalEntries = result.lexicalEntries[i].entries;
-
-      for (let j = 0; j < lexicalEntries.length; j++) {
-        const entry = lexicalEntries[j];
-        let etymology = undefined;
-
-        if (entry.etymologies && entry.etymologies.length) {
-          etymology = entry.etymologies[0];
-        }
-
-        for (let k = 0; k < entry.senses.length; k++) {
-          let domain = undefined;
-          const sense = entry.senses[k];
-          const definition = sense.definitions[0];
-
-          if (sense.domains && sense.domains.length) {
-            domain = sense.domains[0];
-          }
-
-          meanings.push({
-            definition: definition,
-            etymology: etymology,
-            domain: domain
-          });
-        }
-      }
-    }
-
-    console.log(meanings);
-  })
-})
+exports.handler = (event, context) => {
+  const alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.appId = APP_ID;
+  alexa.execute();
+};
