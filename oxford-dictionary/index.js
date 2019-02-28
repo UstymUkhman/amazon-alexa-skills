113b1cacc37ba6d995d3e00ca0cb9c5ca1804ef7
UstymUkhman <ustym.ukhman@gmail.com>
2019-02-28 21:59:11 +0100

    Oxford Dictionary update

--- oxford-dictionary/index.js
+++ oxford-dictionary/index.js
@@ -4,16 +4,67 @@
 
 'use strict';
 
-const APP_ID   = 'amzn1.ask.skill.b0399b08-77f3-42d4-a8e2-dc6b1b3afd3d';
-// const ENDPOINT = 'https://od-api.oxforddictionaries.com/api/v1';
-const Alexa    = require('alexa-sdk');
+const APP_ID = 'amzn1.ask.skill.b0399b08-77f3-42d4-a8e2-dc6b1b3afd3d';
+const KEY    = '83a31615121ef0ea3be016ca80da654c';
+const Alexa  = require('alexa-sdk');
+const https  = require('https');
+const ID     = '85f3fd29';
 
-const HELP_REPROMPT = 'What word do you want to know the meaning?';
-const HELP          = 'You can say, what does some word mean... or you can say what is some word';
-const UNHANDLED     = 'Sorry, I\'m not sure I understood what you asked... ' + HELP_REPROMPT;
-const WELCOME       = 'Hi! ' + HELP_REPROMPT;
-const STOP          = 'Hope it was helpfull!';
-const CANCEL        = 'OK.';
+const HELP_REPROMPT   = 'What word do you want to know the meaning?';
+const HELP            = 'You can say, what does some word mean... or you can say what is some word';
+const UNHANDLED       = 'Sorry, I\'m not sure I understood what you asked... ' + HELP_REPROMPT;
+const ANOTHER_MEANING = 'Say another if you want to hear another meaning of ';
+const NO_MEANING      = 'Sorry, I haven\'t found any meaning of ';
+const WELCOME         = 'Hi! ' + HELP_REPROMPT;
+const STOP            = 'Hope it was helpfull!';
+const CANCEL          = 'OK.';
+const meanings        = [];
+
+let index = 0;
+
+function parseData (data) {
+  const result = JSON.parse(data).results[0];
+  meanings = [];
+
+  for (let i = 0; i < result.lexicalEntries.length; i++) {
+    const lexicalEntries = result.lexicalEntries[i].entries;
+
+    for (let j = 0; j < lexicalEntries.length; j++) {
+      const entry = lexicalEntries[j];
+      let etymology = undefined;
+
+      if (entry.etymologies && entry.etymologies.length) {
+        etymology = entry.etymologies[0];
+      }
+
+      for (let k = 0; k < entry.senses.length; k++) {
+        let domain = undefined;
+        const sense = entry.senses[k];
+        const definition = sense.definitions[0];
+
+        if (sense.domains && sense.domains.length) {
+          domain = sense.domains[0];
+        }
+
+        meanings.push({
+          definition: definition,
+          etymology: etymology,
+          domain: domain
+        });
+      }
+    }
+  }
+}
+
+function playMeaning (that, word) {
+  const meaning = meanings[index];
+  const domain = meaning.domain ? `in ${meaning.domain} ` : '';
+  const etymology = meaning.etymology ? `Etymology: ${meaning.etymology}` : '';
+  const definition = `The definition of ${word} ${domain} is: ${meaning.definition}... ${etymology}`;
+
+  that.response.speak(definition);
+  index++;
+}
 
 const handlers = {
   'LaunchRequest': function () {
@@ -26,10 +77,45 @@ const handlers = {
 
   'GiveDefinitionIntent': function () {
     const word = this.event.request.intent.slots.word.value;
-  
-    this.response.speak('You just said the word ' + word);
-    this.emit(':responseReady');
-    // const _this = this;
+    const _this = this;
+
+    https.get({
+      host    : 'od-api.oxforddictionaries.com',
+      path    : `/api/v1/entries/en/${word}`,
+      method  : 'GET',
+      port    : 443,
+      headers : {
+        'Accept'  : 'application/json',
+        'app_key' : KEY,
+        'app_id'  : ID
+      }
+    }, function (response) {
+      let data = '';
+
+      response.on('data', function (chunk) {
+        data += chunk;
+      })
+
+      .on('end', function () {
+        parseData(data);
+        const last = meanings.length - 1;
+
+        _this.response.shouldEndSession = false;
+
+        if (!meanings.length) {
+          _this.response.speak(NO_MEANING + word);
+        } else {
+          playMeaning(_this, word);
+
+          if (index < last) {
+            _this.response.speak(ANOTHER_MEANING + word);
+          }
+        }
+
+        _this.response.listen();
+        _this.emit(':responseReady');
+      })
+    })
   },
 
   'Unhandled': function () {
